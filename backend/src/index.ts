22a86fefcc2136b069319ff0b3e16ccc0411baca
yuno-qq <ars.yuno@gmail.com>
2025-10-03 17:17:54 +0300

    trpc

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,20 @@
-console.log('hello!!!')
+import express from 'express'
+import * as trpcExpress from "@trpc/server/adapters/express"
+import {trpcRouter} from "../trpc";
+
+const expressApp = express()
+
+expressApp.get('/ping', (req, res) => {
+  res.send('Pong!')
+})
+
+expressApp.use(
+    '/trpc',
+    trpcExpress.createExpressMiddleware({
+      router: trpcRouter
+    })
+)
+
+expressApp.listen(3000, () => {
+  console.info('Listening at http://localhost:3000')
+})
--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,27 +1,9 @@
 export const App = () => {
-  const ideas = [
-    {
-      nick: 'cool-idia-nick-1',
-      name: 'Idea 1',
-      description: 'Idea 1 description...',
-    },
-    {
-      nick: 'cool-idia-nick-2',
-      name: 'Idea 2',
-      description: 'Idea 2 description...',
-    },
-    {
-      nick: 'cool-idia-nick-3',
-      name: 'Idea 3',
-      description: 'Idea 3 description...',
-    },
-  ]
-
   return (
     <div>
       <h1>Idea nick</h1>
 
-      {ideas.map((idea) => (
+      {[{name: 'asd', description: 'adas', nick: 'squad001'}].map((idea) => (
         <div key={idea.nick}>
           <h2>{idea.name}</h2>
           <p>{idea.description}</p>
