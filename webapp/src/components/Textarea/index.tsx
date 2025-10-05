22844aefc15efcff0986cd5677c0faac0eb51420
yuno-qq <ars.yuno@gmail.com>
2025-10-05 14:25:17 +0300

    Textarea

--- webapp/src/components/Textarea/index.tsx
+++ webapp/src/components/Textarea/index.tsx
@@ -1,3 +1,26 @@
-export const Textarea = () => {
-  return <div></div>
+export const Textarea = ({
+  name,
+  label,
+  state,
+  setState,
+}: {
+  name: string
+  label: string
+  state: Record<string, any>
+  setState: React.Dispatch<React.SetStateAction<any>>
+}) => {
+  return (
+    <div style={{ marginBottom: 10 }}>
+      <label htmlFor={name}>{label}</label>
+      <br />
+      <textarea
+        onChange={(e) => {
+          setState({ ...state, [name]: e.target.value })
+        }}
+        value={state[name]}
+        name={name}
+        id={name}
+      />
+    </div>
+  )
 }
--- webapp/src/pages/NewIdeaPage/index.tsx
+++ webapp/src/pages/NewIdeaPage/index.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import { Input } from '../../components/Input'
 import { Segment } from '../../components/Segment'
+import { Textarea } from '../../components/Textarea'
 
 export const NewIdeaPage = () => {
   const [state, setState] = useState({
@@ -21,20 +22,7 @@ export const NewIdeaPage = () => {
         <Input name="name" label="Name" state={state} setState={setState} />
         <Input name="nick" label="Nick" state={state} setState={setState} />
         <Input name="description" label="Description" state={state} setState={setState} />
-
-        <div style={{ marginBottom: 10 }}>
-          <label htmlFor="text">Text</label>
-          <br />
-          <textarea
-            onChange={(e) => {
-              setState({ ...state, text: e.target.value })
-            }}
-            value={state.text}
-            name="text"
-            id="text"
-          />
-        </div>
-
+        <Textarea name="text" label="Text" state={state} setState={setState} />
         <button type="submit">Create Idea</button>
       </form>
     </Segment>
