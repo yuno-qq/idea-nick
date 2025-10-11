d83a6a385ce33fc965c625342a8acbddaa96401b
yuno-qq <ars.yuno@gmail.com>
2025-10-11 17:31:39 +0300

    404 image

--- webapp/src/components/ErrorPageComponent/index.tsx
+++ webapp/src/components/ErrorPageComponent/index.tsx
@@ -4,13 +4,16 @@ import { Segment } from '../Segment'
 export const ErrorPageComponent = ({
   title = 'Oops, error',
   message = 'Something went wrong',
+  children,
 }: {
   title?: string
   message?: string
+  children?: React.ReactNode
 }) => {
   return (
     <Segment title={title}>
       <Alert color="red">{message}</Alert>
+      {children}
     </Segment>
   )
 }
--- webapp/src/pages/other/NotFoundPage/index.tsx
+++ webapp/src/pages/other/NotFoundPage/index.tsx
@@ -1,4 +1,6 @@
+import image404 from '../../../assets/images/404.png'
 import { ErrorPageComponent } from '../../../components/ErrorPageComponent'
+import css from './index.module.scss'
 
 export const NotFoundPage = ({
   title = 'Not Found',
@@ -6,4 +8,8 @@ export const NotFoundPage = ({
 }: {
   title?: string
   message?: string
-}) => <ErrorPageComponent title={title} message={message} />
+}) => (
+  <ErrorPageComponent title={title} message={message}>
+    <img src={image404} alt="" className={css.image} width={800} height={600} />
+  </ErrorPageComponent>
+)
