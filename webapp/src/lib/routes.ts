c34896ab448f8933854ae4d64a1962366f3cf7ad
yuno-qq <ars.yuno@gmail.com>
2025-10-27 20:34:56 +0300

    Sm ref

--- webapp/src/lib/routes.ts
+++ webapp/src/lib/routes.ts
@@ -1,17 +1,10 @@
 import { pgr } from '../utils/pumpGetRoute'
 
 export const getAllIdeasRoute = pgr(() => '/')
-
 export const getViewIdeaRoute = pgr({ ideaNick: true }, ({ ideaNick }) => `/ideas/${ideaNick}`)
-
 export const getEditIdeaRoute = pgr({ ideaNick: true }, ({ ideaNick }) => `/ideas/${ideaNick}/edit`)
-
 export const getNewIdeaRoute = pgr(() => '/ideas/new')
-
 export const getEditProfileRoute = pgr(() => '/edit-profile')
-
 export const getSignUpRoute = pgr(() => '/sign-up')
-
 export const getSignInRoute = pgr(() => '/sign-in')
-
 export const getSignOutPage = pgr(() => '/sign-out')
